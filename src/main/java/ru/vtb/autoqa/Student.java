a7f483e26e96fa4252482db8cbbb4934a4f1864a
Хитрова Ольга Николаевна (4090473) - ООО ГК <o.hitrova@dev.vtb.ru>
2025-11-05 21:22:43 +0300

    +checkGrade

--- src/main/java/ru/vtb/autoqa/Student.java
+++ src/main/java/ru/vtb/autoqa/Student.java
@@ -1,5 +1,14 @@
 package ru.vtb.autoqa;
 
+import lombok.SneakyThrows;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.net.http.HttpClient;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -22,13 +31,6 @@ public class Student {
 
     public List<Integer> getGrades() {
         return new ArrayList<>(grades);
-    }
-
-    public void addGrade(int grade) {
-        if (grade < 2 || grade > 5) {
-            throw new IllegalArgumentException(grade + " is wrong grade");
-        }
-        grades.add(grade);
     }
 
     @Override
@@ -60,5 +62,26 @@ public class Student {
     @Override
     public String toString() {
         return "Student{" + "name=" + name + ", marks=" + grades + '}';
+    }
+
+    @SneakyThrows
+    public void addGrade(int grade) {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet request = new HttpGet("http://localhost:5352/checkGrade?grade="+grade);
+        CloseableHttpResponse httpResponse = httpClient.execute(request);
+        HttpEntity entity = httpResponse.getEntity();
+        if(!Boolean.parseBoolean(EntityUtils.toString(entity))){
+            throw new IllegalArgumentException(grade + " is wrong grade");
+        }
+        grades.add(grade);
+    }
+
+    @SneakyThrows
+    public int raiting() {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet request = new HttpGet("http://localhost:5352/educ?sum="+grades.stream().mapToInt(x->x).sum());
+        CloseableHttpResponse httpResponse = httpClient.execute(request);
+        HttpEntity entity = httpResponse.getEntity();
+        return Integer.parseInt(EntityUtils.toString(entity));
     }
 }
